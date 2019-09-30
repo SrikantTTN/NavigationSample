e30b5671870a40b4dcb3139935b1a1303ad9ac93
srikant.karnani@tothenew.com <srikant.karnani@tothenew.com>
2019-09-30 13:12:47 +0530

    Bottom Sheet with Navigation

--- app/src/main/java/com/example/navigationsample/Navigation.kt
+++ app/src/main/java/com/example/navigationsample/Navigation.kt
@@ -1,12 +1,35 @@
 package com.example.navigationsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_navigation.*
 
 class Navigation : AppCompatActivity() {
+
+    lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
+        val navController = findNavController(this, R.id.navigation_frag)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+
+        // ActionBar
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        //BottomNav
+
+        bottom_appbar.setupWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(this, R.id.navigation_frag)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
